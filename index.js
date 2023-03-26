37cba2ada81cf9a5f94bbd9e03dcaa1e497324d1
ev-krasilnikov <129010378+ev-krasilnikov@users.noreply.github.com>
2023-03-27 01:23:46 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const inputANode = document.querySelector('.js-number-a');  
+const inputBNode = document.querySelector('.js-number-b');  
+const selectOperatioNode = document.querySelector('.js-select-operation');  
+const btnResultNode = document.querySelector('.js-btn-result');  
+const outputNode = document.querySelector('.js-output'); 
+
+btnResultNode.addEventListener('click', function() {
+    const a = Number(inputANode.value);
+    const b = Number(inputBNode.value);
+    const operation = selectOperatioNode.value;
+
+    const result =  calculate({a, b, operation });
+
+outputNode.innerHTML = result;
+    
+
+
+});
